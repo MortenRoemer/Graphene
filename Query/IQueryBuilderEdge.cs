dade886c570d7930a63c8cc47b06cf15633eca53
Morten Römer <morten.roemer@gmx.de>
2020-09-26 16:56:32 +0200

    Finished Basic Query Interface

--- Query/IQueryBuilderEdge.cs
+++ Query/IQueryBuilderEdge.cs
@@ -1,7 +1,18 @@
+using System;
+using System.Collections.Generic;
+
 namespace Graphene.Query
 {
     public interface IQueryBuilderEdge : IQueryBuilder, IQueryFilterable<IQueryBuilderEdge>
     {
-        
+        IQueryBuilderVertex AnyVertices();
+
+        IQueryBuilderVertex SourceVertex();
+
+        IQueryBuilderVertex TargetVertex();
+
+        IQueryBuilderVertex Vertex(Guid id);
+
+        IQueryBuilderVertex Vertices(IEnumerable<Guid> ids);
     }
 }

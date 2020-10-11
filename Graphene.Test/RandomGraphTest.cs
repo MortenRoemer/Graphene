19208ca390f867907b3fd4392935bc228c6057f0
Marethuy <Marvin.Max@gmx.net>
2020-10-11 13:24:34 +0200

    Added RandomGraphGenerator

---
+++ Graphene.Test/RandomGraphTest.cs
@@ -0,0 +1,16 @@
+﻿using Graphene.InMemory;
+using Graphene.Random;
+using Xunit;
+
+namespace Graphene.Test
+{
+    public class RandomGraphTest
+    {
+        [Fact]
+        public void MinimalGraphShouldHaveOneVertex()
+        {
+            var graph = RandomGraphs.GetRandomGraph(1, 0.5d, () => new MemoryGraph(), RandomGraphs.edgeType.hybrid);
+            Assert.Equal(1, graph.Size);
+        }
+    }
+}
